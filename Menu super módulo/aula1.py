ac67bf091b4976f67ac4292e883e85f9dc81d450
ItaloJRPereira <italo.jose14@hotmail.com>
2025-09-19 20:34:47 -0300

    add aula.py

---
+++ Menu super módulo/aula1.py
@@ -0,0 +1,17 @@
+# nome = str (input("digite seu nome")) 
+# idade = int (input("idade"))
+# cidade = str (input("cidade natal?"))
+# comida = str (input("comida favorita?"))
+# cor = str (input("sua cor preferida?"))
+
+# print (f"seu nome é {nome} sua idade {idade} cidade natal {cidade} comida preferida é {comida} e sua cor preferida é {cor} ")
+
+# comprimento = float(input("digite o comprimento"))
+# largura = float (input("digite a largura"))
+
+# print (comprimento*largura)
+
+
+dolar = 5.41
+real = int(input("qual valor ira converter?"))
+print(real/dolar)
